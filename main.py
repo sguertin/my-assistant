18d8f9e2a13c833a79f5ac0e186954ca48f9556b
Scott Guertin <sguertin@housingcenter.com>
2021-05-10 14:09:22 -0400

    Add header to each new file and updated .gitignore

--- main.py
+++ main.py
@@ -29,17 +29,23 @@ if (current_task.exists()):
         for line in f:
             pass
         last_line = line
-    last_hour = int(last_line.partition(':')[0])
+    
+    last_hour = int(last_line.partition('-')[2].partition(':')[0])
     log.info(f'Last hour recorded: {last_hour}:00')
 
+        
 
 if not working_dir.exists():
     log.info(f'Working directory not found at {working_dir}, creating...')
     mkdir(str(working_dir))
     
-def create_entry(date: datetime, entry: str):
-    task_file = get_task_file_path(date)    
+def create_entry(date: datetime, entry: str):    
+    task_file = get_task_file_path(date)
+    task_file_exists = task_file.exists()
     with open(str(task_file), 'a+') as f:
+        if not task_file_exists:
+            log.info(f'Starting new file: {task_file}')
+            f.write(f'---------TimeTracking-{date.year}-{date.month}-{date.day}---------\n')
         f.write(f'{date.hour - 1}:00-{date.hour}:00 - {entry}\n')
     log.info(f'Entry logged for {date.hour}:00')
 
@@ -57,7 +63,7 @@ while True:
         event, values = sg.Window(f'Work Tracking - {now.month}/{now.day}/{now.year}: {now.hour - 1}:00 - {now.hour}:00',
             [
                 [sg.T(f'What have you been working on for {now.hour - 1}:00 - {now.hour}:00?'), sg.In(key='entry')],
-                [sg.B('OK'), sg.B('Cancel') ]
+                [sg.Submit('OK'), sg.Cancel('Cancel') ]
             ]
         ).read(close=True)
         entry = values['entry']
