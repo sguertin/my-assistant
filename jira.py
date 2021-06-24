47ac8388ecc787b7beeeda1c4fd197f9f687a289
Scott Guertin <sguertin@housingcenter.com>
2021-06-24 14:39:52 -0400

    Bug fixes and adding json stringifying for request body

--- jira.py
+++ jira.py
@@ -1,17 +1,19 @@
 import base64
+import json
 import logging
 from logging import Logger
 import requests
 
 from ui.credentials import credentials_prompt
-from ui.warning import warning_retry_prompt
+from ui.warning import warning_prompt, warning_retry_prompt
 
-BASE_URL = 'https://developer.atlassian.com'
+BASE_URL = 'https://jira.housingcenter.com'
 
 def get_auth():
     user_name, password = credentials_prompt()
+    encoding = base64.b64encode(f'{user_name}:{password}'.encode('utf-8'))
     
-    return base64.b64encode(f'{user_name}:{password}'.encode('utf-8'))
+    return str(encoding).replace("b'", "").replace("'", "")
 
 class JiraService:
     auth: str
@@ -35,18 +37,20 @@ class JiraService:
         url = f'{BASE_URL}/rest/api/2/issue/{issue_num}/worklog'
         
         if self.issue_exists(issue_num):
-            response = requests.post(url, headers=self.headers, data={'timeSpent': f'{hours}h'})
+            data = json.dumps({'timeSpent': f'{hours}h'})
+            response = requests.post(url, headers=self.headers, data=data)
             if response.status_code == 403:
                 self.auth = False
                 retry = warning_retry_prompt(f'The credentials provided do not have access to issue {issue_num}, your hours have NOT been logged')
                 if retry:
                     self.log_hours(issue_num, hours)                
             elif response.status_code != 201:
-                self.log.warn(f'Jira replied with a status of {response.status_code}!')
-            
-        
+                warning_msg = f'Jira replied with a status of {response.status_code}!'
+                self.log.warn(warning_msg)
+                warning_prompt(warning_msg)
+
     def issue_exists(self, issue_num: str) -> bool:
         url = f'{BASE_URL}/rest/api/2/issue/{issue_num}'
         
         response = requests.get(url, headers=self.headers)
-        return response.status_code != 404
+        return response.status_code == 200
