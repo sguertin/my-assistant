5aef392bc2704adb9dbabd8c45b3983ab883a45d
Scott Guertin <sguertin@gmail.com>
2021-06-24 23:18:59 -0400

    Refactored main.py to leverage separate modules
    Added settings.ini to .gitignore

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import base64
 from datetime import datetime
 import logging
 import logging.config
-from os import getenv, mkdir
-from pathlib import Path
+from os import mkdir
 from time import sleep
 
 import PySimpleGUIQt as sg
+
+from config import WORKING_DIR
+from issues import get_issues_list
+from jira import JiraResponse, JiraService, NEEDS_AUTH_CODE, FAILED_AUTH
+
+from taskfile import create_tracking_entry, get_last_hour
+from ui.credentials import credentials_prompt
+from ui.time_tracking import record_time
+from ui.warning import warning_prompt, warning_retry_prompt
 
 logging.basicConfig(
     level=logging.CRITICAL,
@@ -17,37 +26,18 @@ sg.theme('DarkBlue3')
 log = logging.getLogger('TimeTracking')
 log.setLevel(logging.INFO)
 
-working_dir = Path(getenv('USERPROFILE'), 'TimeTracking')
-last_hour = 0
-def get_task_file_path(date: datetime) -> Path:
-    return Path(working_dir, f'TimeTracking-{date.year}-{date.month}-{date.day}.log') 
+last_hour = get_last_hour()
 
-current_task = get_task_file_path(datetime.now())
-if (current_task.exists()):
-    log.info(f'Existing tracking found at {current_task}')
-    with open(str(current_task)) as f:
-        for line in f:
-            pass
-        last_line = line
-    
-    last_hour = int(last_line.partition('-')[2].partition(':')[0])
-    log.info(f'Last hour recorded: {last_hour}:00')
+def get_auth():
+    user_name, password = credentials_prompt()
+    encoding = base64.b64encode(f'{user_name}:{password}'.encode('utf-8'))    
+    return encoding.decode()
 
-        
 
-if not working_dir.exists():
-    log.info(f'Working directory not found at {working_dir}, creating...')
-    mkdir(str(working_dir))
-    
-def create_entry(date: datetime, entry: str):    
-    task_file = get_task_file_path(date)
-    task_file_exists = task_file.exists()
-    with open(str(task_file), 'a+') as f:
-        if not task_file_exists:
-            log.info(f'Starting new file: {task_file}')
-            f.write(f'---------TimeTracking-{date.year}-{date.month}-{date.day}---------\n')
-        f.write(f'{date.hour - 1}:00-{date.hour}:00 - {entry}\n')
-    log.info(f'Entry logged for {date.hour}:00')
+if not WORKING_DIR.exists():
+    log.info(f'Working directory not found at {WORKING_DIR}, creating...')
+    mkdir(str(WORKING_DIR))
+
 
 def is_workday(date: datetime) -> bool:
     return date.weekday() < 5
@@ -56,18 +46,58 @@ def is_workhour(date: datetime) -> bool:
     return date.hour > 8 and date.hour <= 17
 
 now = datetime.now()
+
+issues_list, issues_map = get_issues_list()
+
+jira = JiraService(get_auth())
+
 log.info('Initialization is complete, starting...')
-while True:    
-    if is_workday(now) and is_workhour(now) and now.hour != last_hour:
-        last_hour = now.hour
-        event, values = sg.Window(f'Time Tracking',
-            [
-                [sg.T(f'What have you been working on for {now.hour - 1}:00 - {now.hour}:00?')],
-                [sg.In(key='entry')],
-                [sg.Submit('OK'), sg.Cancel('Cancel') ]
-            ]
-        ).read(close=True)
-        entry = values['entry']
-        create_entry(now, entry)
+
+def try_log_work(issue, comment) -> JiraResponse:
+    retry = False
+    response = jira.log_hours(issue['issue_num'], comment)
+    if response.status_code != 201:
+        log.warning(response.message)
+        retry = warning_retry_prompt(response.message)
+        if retry:
+            if response.status_code in [NEEDS_AUTH_CODE, FAILED_AUTH]:
+                jira.auth = get_auth()
+            try_log_work(issue, comment)
+    return response
+
+def main_prompt(timestamp: datetime):
+    issues_list, issues_map = get_issues_list()
+    entry, comment = record_time(issues_list)
+    if entry is not None:
+        if entry not in issues_list: # Check if any new issue entries were added
+            issues_list, issues_map = get_issues_list()
+        try:
+            issue = issues_map[entry]
+            response = try_log_work(issue['issue_num'], comment)
+            log.info(response)
+        except KeyError:
+            log.warning(f'Could not find an issue that matched {entry}')
+        except Exception as ex:
+            log.exception(ex)                
+            warning_prompt(f'An unexpected error occurred posting log to Jira: {ex}')
+        try:
+            create_tracking_entry(timestamp, entry)
+        except Exception as ex:
+            log.exception(ex)
+            warning_prompt(f'An unexpected error occurred writing an entry to the log file: {ex}')
+    return datetime(timestamp.year, timestamp.month, timestamp.day, timestamp.hour + 1, 0, 0)
+
+if last_hour != 0:
+    next = datetime(now.year, now.month, now.day, last_hour + 1, 0, 0)
+else:
+    next = datetime.now(now.year, now.month, now.day, 8, 0, 0)
+
+while True:        
     now = datetime.now()
+    if is_workday(now) and is_workhour(now) and now.hour >= next.hour:
+        next = main_prompt(next)
+        # if Prompt is left open and more than one hour passes
+        # it will iterate through the hours that passed in between 
+        while next.hour < datetime.now().hour and is_workhour(next):
+            next = main_prompt(next)
     sleep(300)
