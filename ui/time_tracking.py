2929ef8d2c52e4e74a1046b13d1449bc4d4fce76
Scott Guertin <sguertin@gmail.com>
2021-06-24 23:18:26 -0400

    Updated Time tracking ui components to be more user friendly

--- ui/time_tracking.py
+++ ui/time_tracking.py
@@ -1,38 +1,51 @@
 from datetime import datetime
 import json
+from pathlib import Path
 
 import PySimpleGUIQt as sg
 
+from issues import add_issue
+
+
 def get_entry_info()-> tuple[str, str]:
-    event, values = sg.Window(f'Time Tracking',
+    issue_field = sg.In(key='-ISSUE-')
+    desc_field = sg.In(key='-DESCRIPTION-')
+    window = sg.Window(f'Time Tracking',
         [
             [sg.T(f'Please provide the Issue information')],
-            [sg.T(f'Issue Number:'), sg.In(key='-ISSUE-')],
-            [sg.T(f'Description:'), sg.In(key='-DESCRIPTION-')],
-            [sg.Submit(), sg.Cancel()]
+            [sg.T(f'Issue Number: '), issue_field],
+            [sg.T(f'Description: '), desc_field],
+            [
+                sg.Button('Save and Add Another', key='Another', bind_return_key=True), 
+                sg.Button('Save and Close', key='Save'), 
+                sg.Cancel('Close')
+            ]
         ]
-    ).read(close=True)
-    if event == 'Submit':
+    )    
+    while True:
+        event, values = window.read()
         issue_num, description = values['-ISSUE-'], values['-DESCRIPTION-']
-        if issue_num is None:
-            return None
+        if event == 'Cancel' or not issue_num:
+            window.close()
+            return None, None
         else:
-            file_entry = { 'issue': issue_num, 'description': description }
-            with open('entries.json', 'r') as f:
-                old_entries: list[dict] = json.loads(f.read())
-            old_entries.append(file_entry)
-            with open('entries.json', 'w') as f:
-                f.write(json.dumps(old_entries))
-        return f'{issue_num} - {description}'
-    elif event == 'Cancel':
-        return None, None
+            issue = { 'issue_num': issue_num, 'description': description }
+            add_issue(issue)                
+            if event == 'Save':
+                window.close()
+                return f'{issue_num} - {description}'
+            elif event == 'Another':
+                issue_field.Update('', select=True)
+                desc_field.Update('')        
+            
 
-def record_time(entries: list[str], now: datetime) -> str:
+def record_time(entries: list[str], now: datetime) -> tuple[str,str]:
     combo = sg.Combo(entries, key='-ENTRY-')
     window = sg.Window(f'Time Tracking',
             [
                 [sg.T(f'What have you been working on for {now.hour - 1}:00 - {now.hour}:00?')],
                 [combo, sg.Button(button_text='Add Entry', key='AddEntry')],
+                [sg.T('Comment (Optional): '), sg.In('-COMMENT-')]
                 [sg.Submit('OK'), sg.Cancel('Cancel') ]
             ]
         )
@@ -40,10 +53,10 @@ def record_time(entries: list[str], now: datetime) -> str:
         event, values = window.read()
         if event == 'Submit':
             window.close()
-            return values['-ENTRY-']
+            return values['-ENTRY-'], values['-COMMENT-']
         elif event == 'Cancel':
             window.close()
-            return None
+            return None, None
         elif event == 'AddEntry':
             new_issue = get_entry_info()
             entries.append(new_issue)
