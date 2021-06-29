8accd4b9f519cecc0a9fd4ceb91ef2cdcb3414fa
Scott Guertin <sguertin@housingcenter.com>
2021-06-29 14:12:58 -0400

    Removed issue management, changed ui to use Issue class

--- ui/time_tracking.py
+++ ui/time_tracking.py
@@ -2,49 +2,17 @@ from datetime import datetime
 
 import PySimpleGUI as sg
 
-from issues import add_issue
+from issues import Issue
+from ui.issues import manage_issues
 
-def get_entry_info(entries: list):
-    issue_field = sg.In(key='-ISSUE-')
-    desc_field = sg.In(key='-DESCRIPTION-')
-    result_field = sg.T(f'                                        ', visible=False)    
-    window = sg.Window(f'Time Tracking - Add New Entry', [
-            [sg.T(f'Please provide the Issue information')],
-            [result_field],
-            [sg.T(f'Issue Number: '), issue_field],
-            [sg.T(f'Description: '), desc_field],
-            [
-                sg.Button('Save and Add Another', key='Another', bind_return_key=True), 
-                sg.Button('Save and Close', key='Save'), 
-                sg.Button('Close')
-            ]
-        ]
-    )    
-    while True:
-        event, values = window.read()        
-        if event in ('Another', 'Save') and values:
-            issue_num, description = values['-ISSUE-'], values['-DESCRIPTION-']
-            if issue_num:
-                issue = { 'issue_num': issue_num, 'description': description }
-                entries.append(add_issue(issue))            
-            if event == 'Save':
-                window.close()
-                break
-            elif event == 'Another':
-                result_field.update(f'Issue {issue_num} was successfully added', visible=True)
-                issue_field.Update('', select=True)
-                desc_field.Update('')        
-        else:
-            window.close()
-            break
 
-def record_time(entries: list[str], timestamp: datetime) -> tuple[str,str]:
+def record_time(entries: list[Issue], timestamp: datetime) -> tuple[Issue, str]:
     combo = sg.Combo(entries, key='-ENTRY-')
     window = sg.Window(f'Time Tracking', [
             [sg.T(f'What have you been working on for {timestamp.hour - 1}:00 - {timestamp.hour}:00?')],
-            [combo, sg.Button(button_text='Add Entry', key='AddEntry')],
+            [combo, sg.Button(button_text='Manage Issues', key='ManageIssues')],
             [sg.T('Comment (Optional): '), sg.In(key='-COMMENT-')],
-            [sg.Submit(), sg.Cancel('Cancel') ],
+            [sg.Submit(), sg.Button('Skip') ],
         ]
     )
     while True:        
@@ -52,9 +20,10 @@ def record_time(entries: list[str], timestamp: datetime) -> tuple[str,str]:
         if event == 'Submit':
             window.close()
             return values['-ENTRY-'], values['-COMMENT-']
-        elif event in ('Cancel', sg.WINDOW_CLOSED):
+        elif event in ('Skip', sg.WINDOW_CLOSED):
             window.close()
             return None, None
-        elif event == 'AddEntry':
-            get_entry_info(entries)
+        elif event == 'ManageIssues':
+            manage_issues(entries)
             combo.Update(values=entries)
+            window.refresh()
