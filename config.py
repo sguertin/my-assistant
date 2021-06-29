9ba583264b0ef0915cb7a8d8cf2004c1b736b4a1
Scott Guertin <sguertin@housingcenter.com>
2021-06-29 14:10:07 -0400

    Added deleted items list, added initialization for issues lists

--- config.py
+++ config.py
@@ -3,16 +3,26 @@ from os import getenv, mkdir
 from pathlib import Path
 from configparser import ConfigParser
 
-log = logging.getLogger('config.py')
+log = logging.getLogger('TimeTracking.config')
 log.setLevel(logging.INFO)
 
 WORKING_DIR: Path = Path(getenv('USERPROFILE'), 'TimeTracking')
 ISSUES_LIST: Path = Path(WORKING_DIR, 'issues.json')
+DELETED_ISSUES_LIST: Path = Path(WORKING_DIR, 'deletedIssues.json')
 SETTINGS_FILE: Path = Path(WORKING_DIR, 'settings.ini')
 
 if not WORKING_DIR.exists():
     log.info(f'Working directory not found at {WORKING_DIR}, creating...')
     mkdir(str(WORKING_DIR))
+
+if not ISSUES_LIST.exists():
+    log.info(f'Issues list not found, creating new list')
+    with open(ISSUES_LIST, 'a') as f:
+        f.write('[]')
+
+if not DELETED_ISSUES_LIST.exists():
+    with open(DELETED_ISSUES_LIST, 'a') as f:
+        f.write('[]')
 
 def create_default_config():
     default_config = ConfigParser()
@@ -30,5 +40,6 @@ if SETTINGS_FILE.exists():
 else:
     configuration = create_default_config()
 
+CONFIG: ConfigParser = configuration
 JIRA_URL: str = configuration.get('JiraSettings', 'base_url', fallback='https://jira.housingcenter.com')
 THEME: str = configuration.get('AppSettings', 'theme', fallback='DarkBlue3')
