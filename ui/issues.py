b30f329070daa6bb796eb12707c512220e8a1ce0
Scott Guertin <sguertin@housingcenter.com>
2021-06-29 14:11:32 -0400

    Moved get_issue_info from time_tracking, finished manage_issues implementation

--- ui/issues.py
+++ ui/issues.py
@@ -1,19 +1,90 @@
 import PySimpleGUI as sg
 
-def manage_issues(issues):
+from issues import add_issue, Issue, save_issues_list, get_deleted_issues
+from ui.warning import warning_ok_cancel_prompt
+
+def get_issue_info(issues: list[Issue]):
+    issue_field = sg.In(key='-ISSUE-')
+    desc_field = sg.In(key='-DESCRIPTION-')
+    result_field = sg.T(f'Issue PRODSUP-00000000 was successfully added', visible=False)    
     window = sg.Window(f'Time Tracking - Add New Entry', [
-        [sg.T(f'Please provide the Issue information')],
-        [sg.Listbox(['Issue 1', 'Issue 2'], key='ToRemove', size=(25, 65)), sg.Frame('',
-            [[sg.Button('+', key='-ADD-')], [sg.Button('-', key='-REMOVE-')]]
-        ), sg.Listbox(['Issue 3', 'Issue 4'], key='ToAdd', size=(25, 65))],
+            [sg.T(f'Please provide the Issue information')],
+            [result_field],
+            [sg.T(f'Issue Number: '), issue_field],
+            [sg.T(f'Description:  '), desc_field],
+            [
+                sg.Button('Save and Add Another', key='Another', bind_return_key=True), 
+                sg.Button('Save and Close', key='Save'), 
+                sg.Button('Close')
+            ]
+        ]
+    )    
+    while True:
+        event, values = window.read()        
+        if event in ('Another', 'Save') and values:
+            issue_num, description = values['-ISSUE-'], values['-DESCRIPTION-']
+            if issue_num:
+                issue = Issue(issue_num, description)
+                add_issue(issue)
+                issues.append(issue)            
+            if event == 'Save':
+                window.close()
+                break
+            elif event == 'Another':
+                result_field.update(f'Issue {issue_num} was successfully added', visible=True)
+                issue_field.Update('', select=True)
+                desc_field.Update('')
+                window.refresh()        
+        else:
+            window.close()
+            break
+
+def manage_issues(issues: list[Issue]):
+    has_changes = False
+    deleted_issues = get_deleted_issues()
+    active_list_box = sg.Listbox(issues, key='ActiveIssues', size=(30, 40))
+    deleted_list_box = sg.Listbox(deleted_issues, key='DeletedIssues', size=(30, 40))
+    window = sg.Window(f'Time Tracking - Manage Issues', [
+        [sg.T('Active Issues', size=(30,1)), sg.T('', size=(5,1)), sg.T('Deleted Issues', size=(30,1))],
+        [
+            active_list_box, 
+            sg.Frame('', [
+                [sg.Button(' + ', key='-ADD-', size=(5,1), tooltip='Add New Issue')], 
+                [sg.Button(' <- ', key='-RESTORE-', size=(5,1), tooltip='Restore Selected Issues')], 
+                [sg.Button(' -> ', key='-REMOVE-', size=(5,1), tooltip='Delete Selected Issues')]
+            ]), 
+            deleted_list_box
+        ],
         [
             sg.Button('Save and Close', key='Save'), 
             sg.Cancel(),
         ]
-    ], size=(500, 750))
+    ], size=(550, 775))
     while True:
         event, values = window.read()
-        print(f'{event=}, {values=}')
-        if event in ('Save', 'Cancel', sg.WINDOW_CLOSED):
+        if event == '-ADD-':
+            get_issue_info(issues)
+            active_list_box.update(issues)
+            has_changes = True
+        elif event == '-REMOVE-':
+            for issue in values['ActiveIssues']:
+                issues.remove(issue)
+                deleted_issues.append(issue)
+            has_changes = True
+        elif event == '-RESTORE-':
+            for issue in values['DeletedIssues']:
+                deleted_issues.remove(issue)
+                issues.append(issue)
+            has_changes = True
+        if event == 'Save' or event in ('Cancel', sg.WINDOW_CLOSE_ATTEMPTED_EVENT) and not has_changes:
+            save_issues_list(issues, deleted_issues)
             window.close()
             break
+        elif event in ('Cancel', sg.WINDOW_CLOSE_ATTEMPTED_EVENT) and has_changes:
+            proceed = warning_ok_cancel_prompt('None of your changes have been saved, are you sure want to continue?')
+            if proceed:
+                window.close()
+                break
+        else:
+            active_list_box.update(issues)
+            deleted_list_box.update(deleted_issues)
