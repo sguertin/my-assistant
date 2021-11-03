d0c5680f9562258ad8a215762d229ae32b4b1ed7
Scott Guertin <sguertin@gmail.com>
2021-11-02 23:10:14 -0400

    Simplified main and removed redundant code

--- main.py
+++ main.py
@@ -2,58 +2,52 @@ import logging
 import logging.config
 from os import mkdir
 
-from my_assistant.providers.authentication import BasicAuthenticationProvider
-from my_assistant.providers.ui import UIProvider
-
 from my_assistant.constants import WORKING_DIR, ISSUES_LIST, DELETED_ISSUES_LIST, SETTINGS_FILE
+from my_assistant.interfaces.launcher import ILauncherService
 from my_assistant.models.settings import Settings
-from my_assistant.services.assistant import Assistant
-from my_assistant.services.time_tracking import get_time_tracking_service
-from my_assistant.ui.warning import warning_prompt, warning_retry_prompt
-from my_assistant.ui.credentials import credentials_prompt
-from my_assistant.ui.time_tracking import record_time
-from my_assistant.ui.launcher import Launcher
+from my_assistant.providers.dependencies import Factory
 
 
 logging.basicConfig(
-    level=logging.CRITICAL,
+    level=logging.CRITICAL,  # Keeps logging in 3rd party packages quiet
     format='%(name)-15s %(message)s',
     datefmt='[%Y-%m-%d %H:%M:%S]',
 )
-log = logging.getLogger('Main')
+log = logging.getLogger('ui_assistant.main')
+log.setLevel(logging.ERROR)
 
 if not WORKING_DIR.exists():
-    log.info(f'Working directory not found at {WORKING_DIR}, creating...')
     mkdir(str(WORKING_DIR))
 
+if not SETTINGS_FILE.exists():
+    settings = Settings.restore_defaults()
+else:
+    try:
+        settings = Settings.load()
+    except Exception as e:
+        log.exception(e)
+        log.error(f'Unable to load settings error: {e} , regenerating...')
+        settings = Settings.restore_defaults()
+
+log.setLevel(settings.log_level)
+
 if not ISSUES_LIST.exists():
-    log.info(f'Issues list not found, creating new list')
+    log.info(f'Issues list: "{ISSUES_LIST}" not found, creating new list')
     with open(ISSUES_LIST, 'w') as f:
         f.write('[]')
 
 if not DELETED_ISSUES_LIST.exists():
+    log.info(
+        f'Deleted Issues list: "{DELETED_ISSUES_LIST}" not found, creating new list')
     with open(DELETED_ISSUES_LIST, 'w') as f:
         f.write('[]')
 
-if not SETTINGS_FILE.exists():
-    with open(SETTINGS_FILE, 'w') as f:
-        f.write(Settings().to_json())
-
-auth_provider = BasicAuthenticationProvider()
-
-
-def create_dependencies() -> 'tuple[Assistant, Settings]':
-    settings = Settings.load()
-    ui_provider = UIProvider(
-        warning_prompt, warning_retry_prompt, record_time, credentials_prompt)
-    time_tracking = get_time_tracking_service(
-        auth_provider, ui_provider, settings)
-    assistant = Assistant(time_tracking, ui_provider, settings)
-    return assistant, settings
-
+factory = Factory(settings)
 
 try:
-    launcher = Launcher(create_dependencies())
-    launcher.run_main_window(create_dependencies)
-except Exception as e:
-    log.error(e)
+    log.info("Initializing Launcher")
+    launcher: ILauncherService = factory.get_launcher_service()
+    log.info("Starting application")
+    launcher.run_main_window()
+except Exception as ex:
+    log.exception(ex)
