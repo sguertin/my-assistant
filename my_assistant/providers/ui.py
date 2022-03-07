7e2cd607da8a064a596514e42c01346b6cd66576
Scott Guertin <sguertin@housingcenter.com>
2022-03-07 14:30:55 -0500

    Renaming methods

--- my_assistant/providers/ui.py
+++ my_assistant/providers/ui.py
@@ -12,62 +12,62 @@ from my_assistant.models.settings import Settings
 
 
 class UIFacadeService:
-    ui_warning: IUIWarningService
-    time_tracking_service: IUITimeTrackingService
-    credentials_service: IUICredentialsService
-    issue_service: IUIIssueService
-    theme_service: IUIThemeService
-    settings_service: IUISettingsService
+    ui_warning_service: IUIWarningService
+    ui_time_tracking_service: IUITimeTrackingService
+    ui_credentials_service: IUICredentialsService
+    ui_issue_service: IUIIssueService
+    ui_theme_service: IUIThemeService
+    ui_settings_service: IUISettingsService
 
     def __init__(
         self,
-        ui_warning: IUIWarningService,
-        time_tracking_service: IUITimeTrackingService,
-        credentials_service: IUICredentialsService,
-        issue_service: IUIIssueService,
-        theme_service: IUIThemeService,
-        settings_service: IUISettingsService,
+        ui_warning_service: IUIWarningService,
+        ui_time_tracking_service: IUITimeTrackingService,
+        ui_credentials_service: IUICredentialsService,
+        ui_issue_service: IUIIssueService,
+        ui_theme_service: IUIThemeService,
+        ui_settings_service: IUISettingsService,
     ):
         """Initializes an instance of the UIProvider
 
         Args:
             ui_warning (IUIWarningService): the UI warning service
-            time_tracking_service (IUITimeTrackingService): the UI time tracking service
-            credentials_service (IUICredentialsService): the UI credentials service
-            issue_service (IUIIssueService): the UI issue service
-            theme_service (IUIThemeService): the UI theme service
-            settings_service (IUISettingsService): the UI settings service
+            ui_time_tracking_service (IUITimeTrackingService): the UI time tracking service
+            ui_credentials_service (IUICredentialsService): the UI credentials service
+            ui_issue_service (IUIIssueService): the UI issue service
+            ui_theme_service (IUIThemeService): the UI theme service
+            ui_settings_service (IUISettingsService): the UI settings service
         """
-        self.ui_warning = ui_warning
-        self.time_tracking_service = time_tracking_service
-        self.credentials_service = credentials_service
-        self.issue_service = issue_service
-        self.theme_service = theme_service
-        self.settings_service = settings_service
+        self.ui_warning_service = ui_warning_service
+        self.ui_time_tracking_service = ui_time_tracking_service
+        self.ui_credentials_service = ui_credentials_service
+        self.ui_issue_service = ui_issue_service
+        self.ui_theme_service = ui_theme_service
+        self.ui_settings_service = ui_settings_service
 
     def warning_retry_prompt(self, msg: str) -> bool:
-        return self.ui_warning.warning_retry_prompt(msg)
+        return self.ui_warning_service.warning_retry_prompt(msg)
 
     def warning_prompt(self, msg: str) -> None:
-        self.ui_warning.warning_prompt(msg)
+        self.ui_warning_service.warning_prompt(msg)
 
     def record_time(self, timestamp: datetime) -> tuple[Issue, str]:
-        return self.time_tracking_service.record_time(timestamp)
+        return self.ui_time_tracking_service.record_time(timestamp)
 
     def credentials_prompt(self) -> tuple[str, str]:
-        return self.credentials_service.credentials_prompt()
+        return self.ui_credentials_service.credentials_prompt()
 
     def get_issue_info(self, issues: list[Issue]) -> list[Issue]:
-        return self.issue_service.get_issue_info(issues)
+        return self.ui_issue_service.get_issue_info(issues)
 
     def manage_issues(self) -> tuple[bool, list[Issue]]:
-        return self.issue_service.manage_issues()
+        return self.ui_issue_service.manage_issues()
 
     def manage_theme(self, settings: Settings) -> Settings:
-        return self.theme_service.manage_theme(settings)
+        return self.ui_theme_service.manage_theme(settings)
 
     def change_settings(self, settings: Settings) -> Settings:
-        return self.settings_service.change_settings(settings)
+        return self.ui_settings_service.change_settings(settings)
 
     def set_theme(self, new_theme: str) -> None:
-        self.settings_service.set_theme(new_theme)
+        self.ui_settings_service.set_theme(new_theme)
