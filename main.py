b970f05ab2d35def2226eb11c86c929600c2b2dc
Scott Guertin <sguertin@housingcenter.com>
2022-03-07 14:35:06 -0500

    Formatting and refactoring for dependencies

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 from os import mkdir
-from models.settings import Settings
 
-from my_assistant.constants import WORKING_DIR, ISSUES_LIST, DELETED_ISSUES_LIST, SETTINGS_FILE, LogLevel
+from my_assistant.constants import (
+    WORKING_DIR,
+    ISSUES_LIST,
+    DELETED_ISSUES_LIST,
+    SETTINGS_FILE,
+)
 from my_assistant.factories.dependencies import DependencyFactory
 from my_assistant.factories.logging import LoggingFactory
-
+from my_assistant.interfaces.factories.dependencies import IDependencyFactory
+from my_assistant.models.settings import Settings
 from my_assistant.services.ui.launcher import LauncherService
 
-log = LoggingFactory(LogLevel.INFO).get_logger('Main')
+log = LoggingFactory().get_logger("Main")
 
 if not WORKING_DIR.exists():
-    log.info(f'Working directory not found at {WORKING_DIR}, creating...')
+    log.info(f"Working directory not found at {WORKING_DIR}, creating...")
     mkdir(str(WORKING_DIR))
 
 if not ISSUES_LIST.exists():
-    log.info(f'Issues list not found, creating new list')
-    with open(ISSUES_LIST, 'w') as f:
-        f.write('[]')
+    log.info(f"Issues list not found, creating new list")
+    with open(ISSUES_LIST, "w") as f:
+        f.write("[]")
 
 if not DELETED_ISSUES_LIST.exists():
-    with open(DELETED_ISSUES_LIST, 'w') as f:
-        f.write('[]')
+    with open(DELETED_ISSUES_LIST, "w") as f:
+        f.write("[]")
 
 if not SETTINGS_FILE.exists():
-    with open(SETTINGS_FILE, 'w') as f:
+    with open(SETTINGS_FILE, "w") as f:
         f.write(Settings().to_json())
 
-
+dependency_factory: IDependencyFactory = DependencyFactory()
 try:
-    assistant, ui_provider, settings = DependencyFactory.create_dependencies()
-    launcher = LauncherService(assistant, ui_provider, settings)
+    assistant, ui_provider, settings = dependency_factory.create_dependencies()
+    launcher = LauncherService(assistant, ui_provider, dependency_factory, settings)
     launcher.run_main_window(lambda: DependencyFactory.create_dependencies())
 except Exception as e:
     log.error(e)
