97f76cf7e447ac707e1cd184c163d53a0e88e00f
Scott Guertin <sguertin@gmail.com>
2022-03-10 13:57:48 -0500

    Add try/except for WORKING_DIR to use 'env:HOME' is 'env:USERPROFILE' is empty

--- my_assistant/constants.py
+++ my_assistant/constants.py
@@ -3,29 +3,33 @@ from logging import CRITICAL, ERROR, WARNING, INFO, DEBUG
 from os import getenv
 from pathlib import Path
 
-WORKING_DIR: Path = Path(getenv('USERPROFILE'), 'TimeTracking')
-ISSUES_LIST: Path = Path(WORKING_DIR, 'issues.json')
-DELETED_ISSUES_LIST: Path = Path(WORKING_DIR, 'deletedIssues.json')
-SETTINGS_FILE: Path = Path(WORKING_DIR, 'settings.json')
+try:
+    WORKING_DIR: Path = Path(getenv("USERPROFILE"), "TimeTracking")
+except:
+    WORKING_DIR: Path = Path(getenv("HOME"), "TimeTracking")
+
+ISSUES_LIST: Path = Path(WORKING_DIR, "issues.json")
+DELETED_ISSUES_LIST: Path = Path(WORKING_DIR, "deletedIssues.json")
+SETTINGS_FILE: Path = Path(WORKING_DIR, "settings.json")
 
 HOUR_RANGE: range = range(24)
 MINUTE_RANGE: range = range(60)
 
 DAYS_OF_WEEK = {
-    'Monday': 0,
-    'Tuesday': 1,
-    'Wednesday': 2,
-    'Thursday': 3,
-    'Friday': 4,
-    'Saturday': 5,
-    'Sunday': 6,
+    "Monday": 0,
+    "Tuesday": 1,
+    "Wednesday": 2,
+    "Thursday": 3,
+    "Friday": 4,
+    "Saturday": 5,
+    "Sunday": 6,
 }
 LOGGING_LEVELS = {
-    'Critical': CRITICAL,
-    'Error': ERROR,
-    'Warning': WARNING,
-    'Info': INFO,
-    'Debug': DEBUG,
+    "Critical": CRITICAL,
+    "Error": ERROR,
+    "Warning": WARNING,
+    "Info": INFO,
+    "Debug": DEBUG,
 }
 
 
