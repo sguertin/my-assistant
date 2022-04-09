147672f94581526906a213990349dfbbd802286d
Scott Guertin <sguertin@gmail.com>
2022-04-09 12:03:35 -0400

    Add logging and remove redundant functions

--- my_assistant/services/taskfile.py
+++ my_assistant/services/taskfile.py
@@ -1,13 +1,20 @@
 from datetime import datetime, timedelta, time
 from json import JSONDecodeError
+from logging import Logger
 from pathlib import Path
 from typing import Optional
 
 from my_assistant.constants import WORKING_DIR
+from my_assistant.interfaces.logfactory import ILoggingFactory
 from my_assistant.models.taskfile import TimeDayLog, TimeEntry
 
 
 class TaskFileService:
+    log: Logger
+
+    def __init__(self, log_factory: ILoggingFactory):
+        self.log = log_factory.get_logger("TaskFileService")
+
     @staticmethod
     def get_time_log_path(timestamp: datetime) -> Path:
         return Path(
@@ -23,7 +30,8 @@ class TaskFileService:
             else:
                 with open(task_file, "r") as f:
                     task_log = TimeDayLog.from_json(f.read())
-        except JSONDecodeError:
+        except JSONDecodeError as err:
+            self.log.error(err)
             task_log = TimeDayLog()
         return task_log
 
@@ -35,43 +43,15 @@ class TaskFileService:
         task_log.time_entries.append(
             TimeEntry(timestamp - time_interval, timestamp, entry)
         )
-        with open(self.get_time_log_path(timestamp), "w") as f:
-            f.write(task_log.to_json())
+        try:
+            with open(self.get_time_log_path(timestamp), "w") as f:
+                f.write(task_log.to_json())
+        except IOError as err:
+            self.log.error(err)
+            raise
 
     def get_last_entry_time(self, timestamp: datetime) -> Optional[time]:
         task_log = self.get_time_log(timestamp)
         if len(task_log.time_entries) > 0:
             return task_log.time_entries[-1].end_time
         return None
-
-
-def get_time_log_path(timestamp: datetime) -> Path:
-    return Path(
-        WORKING_DIR,
-        f"TimeTracking-{timestamp.year}-{timestamp.month}-{timestamp.day}.log",
-    )
-
-
-def get_time_log(timestamp: datetime) -> TimeDayLog:
-    task_file = get_time_log_path(timestamp)
-    if not task_file.exists():
-        task_log = TimeDayLog()
-    else:
-        with open(task_file, "r") as f:
-            task_log = TimeDayLog.from_json(f.read())
-    return task_log
-
-
-def create_tracking_entry(timestamp: datetime, entry: str, time_interval: timedelta):
-    task_log: TimeDayLog = get_time_log(timestamp)
-
-    task_log.time_entries.append(TimeEntry(timestamp - time_interval, timestamp, entry))
-    with open(get_time_log_path(timestamp), "w") as f:
-        f.write(task_log.to_json())
-
-
-def get_last_entry_time(timestamp: datetime) -> time:
-    task_log = get_time_log(timestamp)
-    if len(task_log.time_entries) > 0:
-        return task_log.time_entries[-1].end_time
-    return None
