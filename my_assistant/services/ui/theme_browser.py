928ea84da4ae8f1d4c7f890ac28a3bf466bc6b55
Scott Guertin <sguertin@gmail.com>
2022-04-09 11:18:27 -0400

    Formatting updates

--- my_assistant/services/ui/theme_browser.py
+++ my_assistant/services/ui/theme_browser.py
@@ -4,26 +4,31 @@ from my_assistant.models.settings import Settings
 
 
 class UIThemeService:
-
     def manage_theme(self, settings: Settings) -> Settings:
         layout = [
-            [sg.Text('Theme Browser')],
-            [sg.Text('Click a Theme color to see demo window')],
-            [sg.Listbox(values=sg.theme_list(), size=(20, 12),
-                        key='-LIST-', enable_events=True)],
-            [sg.Button('Update'), sg.Button('Exit')]
+            [sg.Text("Theme Browser")],
+            [sg.Text("Click a Theme color to see demo window")],
+            [
+                sg.Listbox(
+                    values=sg.theme_list(),
+                    size=(20, 12),
+                    key="-LIST-",
+                    enable_events=True,
+                )
+            ],
+            [sg.Button("Update"), sg.Button("Exit")],
         ]
 
-        window = sg.Window('Theme Browser', layout)
+        window = sg.Window("Theme Browser", layout)
 
         while True:  # Event Loop
             event, values = window.read()
-            if event in (sg.WIN_CLOSED, 'Exit'):
+            if event in (sg.WIN_CLOSED, "Exit"):
                 break
-            new_theme = values['-LIST-'][0]
+            new_theme = values["-LIST-"][0]
             sg.theme(new_theme)
-            sg.popup_get_text(f'This is {new_theme}')
-            if event == 'Update':
+            sg.popup_get_text(f"This is {new_theme}")
+            if event == "Update":
                 if settings.theme != new_theme:
                     settings.theme = new_theme
                     settings.save()
