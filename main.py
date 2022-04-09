73792e41c85ff32ced930c883f1a7e6c39d70d15
Scott Guertin <sguertin@gmail.com>
2022-04-09 14:47:33 -0400

    Simplify main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from my_assistant.constants import LogLevel
 from my_assistant.factories.dependencies import DependencyFactory
-from my_assistant.factories.logfactory import LoggingFactory
+from my_assistant.factories.log_factory import LoggingFactory
 from my_assistant.interfaces.factories.dependencies import IDependencyFactory
 from my_assistant.interfaces.ui.launcher import IUILauncherService
-from my_assistant.services.ui.launcher import UILauncherService
 
 log = LoggingFactory(LogLevel.INFO).get_logger("Main")
 
 dependency_factory: IDependencyFactory = DependencyFactory()
 try:
     log.info("Starting My Assistant...")
-    assistant, ui_provider, settings_service = dependency_factory.create_dependencies()
-    launcher: IUILauncherService = UILauncherService(
-        assistant, ui_provider, dependency_factory, settings_service
-    )
+    launcher: IUILauncherService = dependency_factory.create_launcher()
     launcher.run_main_window()
     log.info("Closing My Assistant...")
 except Exception as e:
