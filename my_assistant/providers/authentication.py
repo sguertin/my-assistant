e6d412a85f5ca10f47564a020c374fa77896e5d6
Scott Guertin <sguertin@gmail.com>
2022-04-09 12:01:49 -0400

    Add interface to BasicAuthenticationProvider

--- my_assistant/providers/authentication.py
+++ my_assistant/providers/authentication.py
@@ -1,16 +1,19 @@
 import base64
 
-class BasicAuthenticationProvider:
+from my_assistant.interfaces.authentication import IAuthenticationProvider
+
+
+class BasicAuthenticationProvider(IAuthenticationProvider):
     _auth: bytes = None
 
-    def clear_auth(self)->None:
+    def clear_auth(self) -> None:
         self._auth = None
 
     def get_auth(self) -> str:
         if self._auth is not None:
-            return f'Basic {self._auth.decode()}'
+            return f"Basic {self._auth.decode()}"
         return None
 
     def set_auth(self, user_name: str, password: str):
-        encoding = base64.b64encode(f'{user_name}:{password}'.encode('utf-8'))    
-        self._auth = encoding        
+        encoding = base64.b64encode(f"{user_name}:{password}".encode("utf-8"))
+        self._auth = encoding
