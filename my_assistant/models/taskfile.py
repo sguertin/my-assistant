20bd89a182c45b9e9d26dc7a9172404ba3366c42
Scott Guertin <sguertin@housingcenter.com>
2022-08-04 13:16:44 -0400

    Set taskfile start and end entries to datetime for JSON

--- my_assistant/models/taskfile.py
+++ my_assistant/models/taskfile.py
@@ -6,13 +6,13 @@ from dataclasses_json import DataClassJsonMixin
 
 @dataclass
 class TimeEntry(DataClassJsonMixin):
-    start_time: time
-    end_time: time
+    start: datetime
+    end: datetime
     entry: str
 
-    def __init__(self, start_time: datetime, end_time: datetime, entry: str):
-        self.start_time = start_time.time()
-        self.end_time = end_time.time()
+    def __init__(self, start: datetime, end: datetime, entry: str):
+        self.start = start
+        self.end = end
         self.entry = entry
 
 
@@ -24,6 +24,9 @@ class TimeDayLog(DataClassJsonMixin):
 
     def __init__(self):
         today = datetime.today()
-        self.log_date = today
+        self.log_date = datetime(
+            year=today.year, month=today.month, day=today.day,
+            hour=0, minute=0, second=0, microsecond=0
+        )
         self.time_entries = []
         self.file_name = f"TimeTracking-{today.year}-{today.month}-{today.day}.log"
